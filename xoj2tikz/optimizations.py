47092e29f92ae2f166fde8e7ab325c44485da10d
Simon Vetter <xoj2tikz@powerswitch-entertainment.de.vu>
2012-02-28 08:27:32 +0100

    working stroke line optimization, attempt to detect circles

--- xoj2tikz/optimizations.py
+++ xoj2tikz/optimizations.py
@@ -18,6 +18,7 @@
 # along with xoj2tikz.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import math
 
 from .page import Page
 from .layer import Layer
@@ -32,15 +33,41 @@ improve the quality of the output file.
 """
 
 def detectCircle(stroke):
-    pass
+    """
+    This function detects circles and converts these strokes.
+    """
+    s = 0
+    if len(stroke.coordList) > 3 and stroke.coordList[-1] == stroke.coordList[0]:
+        originX = stroke.coordList[0][0] + (stroke.coordList[math.ceil((len(stroke.coordList)-1)/2)][0] - stroke.coordList[0][0]) / 2
+        originY = stroke.coordList[0][1] + (stroke.coordList[math.ceil((len(stroke.coordList)-1)/2)][1] - stroke.coordList[0][1]) / 2
+        radius = math.sqrt((originX - stroke.coordList[0][0]) ** 2 + (originY - stroke.coordList[0][1]) ** 2)
+        while (s < len(stroke.coordList)-2): 
+            if math.fabs(radius - math.sqrt((originX - stroke.coordList[s][0]) ** 2 + (originY - stroke.coordList[s][1]) ** 2)) > 2:
+                return
+            s += 1
+        # Circle(stroke.color, originX, originY, radius, stroke.width);
 
 def detectRectangle(stroke):
     pass
 
 def simplifyStrokes(stroke):
-    pass
-
-
+    """
+    This function combines collinear Strokes into one single Stroke.
+    """
+    s = 0
+    while (s < len(stroke.coordList)-2): 
+        c = math.fabs((((stroke.coordList[s][0]-stroke.coordList[s+1][0])*(stroke.coordList[s+1][0]-stroke.coordList[s+2][0])+(stroke.coordList[s][1]-stroke.coordList[s+1][1])*(stroke.coordList[s+1][1]-stroke.coordList[s+2][1]))/(math.sqrt((stroke.coordList[s][0]-stroke.coordList[s+1][0])*(stroke.coordList[s][0]-stroke.coordList[s+1][0])+(stroke.coordList[s][1]-stroke.coordList[s+1][1])*(stroke.coordList[s][1]-stroke.coordList[s+1][1])) * math.sqrt((stroke.coordList[s+1][0]-stroke.coordList[s+2][0])*(stroke.coordList[s+1][0]-stroke.coordList[s+2][0])+(stroke.coordList[s+1][1]-stroke.coordList[s+2][1])*(stroke.coordList[s+1][1]-stroke.coordList[s+2][1])))))
+        if (c > 0.998):
+            if (len(stroke.coordList[s+1]) == 3):
+                if (math.fabs(stroke.coordList[s][2] - stroke.coordList[s+1][2]) < 0.1 and math.fabs(stroke.coordList[s+2][2] - stroke.coordList[s+1][2]) < 0.1):
+                    del stroke.coordList[s+1]
+                else:
+                    s += 1
+            else:
+                    del stroke.coordList[s+1]
+        else:
+            s += 1
+        
 def runAll(obj):
     """
     This is a recursive function. You can throw any list or instance of
